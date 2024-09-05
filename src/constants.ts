e9cba751fda8c3382ce07a66a3c20f6681aedae8
KRIPOTA <dyupin.lesha@yandex.ru>
2024-09-05 21:06:49 +0700

    ранжирование вопросов по типам

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,2 @@
-export const MONGO_URI = 'mongodb://mongodb/dev_ops_test'; //'mongodb://127.0.0.1/dev_ops_test'
+// export const MONGO_URI = 'mongodb://127.0.0.1/dev_ops_test';
+export const MONGO_URI = 'mongodb://mongodb/dev_ops_test';
--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -18,8 +18,12 @@ export class QuestionsService {
     date.setHours(0, 0, 0, 0);
 
     const questionsIds = await this.getQuestionsIdsByDate(date);
+    const questions = await this.questionModel.find({
+      _id: { $in: questionsIds },
+    });
 
-    return this.questionModel.find({ _id: { $in: questionsIds } });
+    const shuffledQuestions = this.shuffleArray([...questions]);
+    return shuffledQuestions;
   }
 
   async getQuestionsIdsByDate(date: Date) {
@@ -80,6 +84,14 @@ export class QuestionsService {
     return { shuffledArr, newIndex };
   }
 
+  private shuffleArray(arr: any[]) {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+  }
+
   async deleteQuestionsByDate() {
     const date = new Date();
     date.setHours(0, 0, 0, 0);
@@ -88,43 +100,107 @@ export class QuestionsService {
   }
 
   async getFresh(questionLength = 20) {
-    // Получаем вопросы без даты публикации
-    let questions =
-      (await this.questionModel.find({
-        datePublishEveryDay: { $eq: null },
-      })) || [];
+    const questions = [];
+    const tags = [];
 
-    if (questions.length < questionLength) {
-      // Получаем вопросы с датой публикации более двух месяцев назад
-      const twoMonthsAgo = new Date();
-      twoMonthsAgo.setMonth(twoMonthsAgo.getMonth() - 2);
-      const questionsWithOldDate = await this.questionModel.find({
-        datePublishEveryDay: { $lt: twoMonthsAgo },
+    while (questions.length < questionLength) {
+      //Получаем вопрос с датой публикации равной null и тегами, которых не было
+      let question = await this.questionModel.findOne({
+        _id: { $nin: questions.map((q) => q._id) },
+        datePublishEveryDay: { $eq: null },
+        tags: { $nin: tags },
       });
 
-      questions = questions.concat(questionsWithOldDate);
-
-      if (questions.length < questionLength) {
-        // Получаем случайные вопросы, исключая те, которые уже есть в предыдущих массивах
-        const randomQuestions = await this.questionModel.aggregate([
-          {
-            $match: {
-              _id: {
-                $nin: questions.map((q) => q._id),
-              },
-            },
-          },
-          {
-            $sample: {
-              size: questionLength - questions.length,
-            },
-          },
-        ]);
-
-        questions = questions.concat(randomQuestions);
+      //Получаем вопрос с датой публикации более двух месяцев назад и тегами, которых не было
+      if (!question) {
+        const twoMonthsAgo = new Date();
+        twoMonthsAgo.setMonth(twoMonthsAgo.getMonth() - 2);
+        question = await this.questionModel.findOne({
+          _id: { $nin: questions.map((q) => q._id) },
+          datePublishEveryDay: { $lt: twoMonthsAgo },
+          tags: { $nin: tags },
+        });
       }
+
+      //Получаем вопрос с датой публикации равной null
+      if (!question) {
+        question = await this.questionModel.findOne({
+          _id: { $nin: questions.map((q) => q._id) },
+          datePublishEveryDay: { $eq: null },
+        });
+      }
+
+      //Получаем вопрос с датой публикации более двух месяцев назад
+      if (!question) {
+        const twoMonthsAgo = new Date();
+        twoMonthsAgo.setMonth(twoMonthsAgo.getMonth() - 2);
+        question = await this.questionModel.findOne({
+          _id: { $nin: questions.map((q) => q._id) },
+          datePublishEveryDay: { $lt: twoMonthsAgo },
+        });
+      }
+
+      //Получаем вопрос с тегами, которых не было
+      if (!question) {
+        question = await this.questionModel.findOne({
+          _id: { $nin: questions.map((q) => q._id) },
+          tags: { $nin: tags },
+        });
+      }
+
+      //Получаем вопрос, которого нет в массиве
+      if (!question) {
+        question = await this.questionModel.findOne({
+          _id: { $nin: questions.map((q) => q._id) },
+        });
+      }
+
+      if (!question) break;
+
+      questions.push(question);
+      tags.push(question.tags[3] || question.tags[2] || '');
     }
-    return questions.slice(0, questionLength);
+
+    return questions;
+
+    // Старый метод
+    // Получаем вопросы без даты публикации
+    // let questions =
+    //   (await this.questionModel.find({
+    //     datePublishEveryDay: { $eq: null },
+    //   })) || [];
+
+    // if (questions.length < questionLength) {
+    //   // Получаем вопросы с датой публикации более двух месяцев назад
+    //   const twoMonthsAgo = new Date();
+    //   twoMonthsAgo.setMonth(twoMonthsAgo.getMonth() - 2);
+    //   const questionsWithOldDate = await this.questionModel.find({
+    //     datePublishEveryDay: { $lt: twoMonthsAgo },
+    //   });
+
+    //   questions = questions.concat(questionsWithOldDate);
+
+    //   if (questions.length < questionLength) {
+    //     // Получаем случайные вопросы, исключая те, которые уже есть в предыдущих массивах
+    //     const randomQuestions = await this.questionModel.aggregate([
+    //       {
+    //         $match: {
+    //           _id: {
+    //             $nin: questions.map((q) => q._id),
+    //           },
+    //         },
+    //       },
+    //       {
+    //         $sample: {
+    //           size: questionLength - questions.length,
+    //         },
+    //       },
+    //     ]);
+
+    //     questions = questions.concat(randomQuestions);
+    //   }
+    // }
+    // return questions.slice(0, questionLength);
   }
 
   async create(dto: CreateQuestionDto[]) {
